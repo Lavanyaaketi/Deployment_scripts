bece80c417ac4de9940721bed3e584b57a8b0fcc
Lavanya Aketi <arlavanya28.com>
2024-08-04 11:47:29 -0500

    Add automation

---
+++ Automation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+export AWS_DEFAULT_REGION=us-east-2
+: '
+# create target group
+aws elbv2 create-target-group \
+    --name my-targets \
+    --protocol HTTP \
+    --port 80 \
+    --target-type instance \
+    --vpc-id vpc-009aa2f325fd297d1
+    
+     # register target group
+    aws elbv2 register-targets \
+    --target-group-arn arn:aws:elasticloadbalancing:us-east-2:590183815392:targetgroup/my-targets/c1d2b1b0754f535c \
+    --targets Id=i-060698fe224b7be6e Id=i-060698fe224b7be6e
+    
+
+    #create load balancer
+
+    aws elbv2 create-load-balancer --name my-load-balancer  \
+--subnets subnet-04b311b955ec3aa0a subnet-0cc0f4e901dc45c10 --security-groups sg-0b23907abc15b2290
+
+   
+    # create listener
+    aws elbv2 create-listener --load-balancer-arn arn:aws:elasticloadbalancing:us-east-2:590183815392:loadbalancer/app/my-load-balancer/2a136064ae62ffb7 \
+--protocol HTTP --port 80  \
+--default-actions Type=forward,TargetGroupArn=arn:aws:elasticloadbalancing:us-east-2:590183815392:targetgroup/my-targets/c1d2b1b0754f535c
+ '
+    # verify health of registered TG
+    aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:us-east-2:590183815392:targetgroup/my-targets/c1d2b1b0754f535c
+    
+
